c8eb29ea94997fc010da35cc6ad23017da3d4f82
David Gros <dngros@live.com>
2020-11-28 21:31:18 -0600

    add a test of weighted grammar gen

--- tests/test_gram.py
+++ tests/test_gram.py
@@ -10,6 +10,13 @@ class Onchoice(SimpleGramChoice):
     ]
 
 
+class Onchoiceweight(SimpleGramChoice):
+    choices = [
+        "foo",
+        ("bar", 5),
+    ]
+
+
 def test_onesampler():
     gram = Grammar(Onchoice)
     n = 10000
@@ -18,5 +25,15 @@ def test_onesampler():
     for count in gen.values():
         expected = n / 3
         assert expected - n * 0.05 < count < expected + n * 0.05
+
+
+def test_onesampler_weight():
+    gram = Grammar(Onchoiceweight)
+    n = 10000
+    gen = Counter(gram.generate_rand_iter(n))
+    assert set(gen.keys()) == {"foo", "bar"}
+    margin = n * 0.05
+    assert n / 6 * 5 - margin < gen['bar'] < n / 6 * 5 + margin
+    assert n / 6 * 1 - margin < gen['foo'] < n / 6 * 1 + margin
 
 
